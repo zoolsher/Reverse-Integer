4b8a47fb87cf761c6765c78b7650054236de7044
zoolsher <zoolsher@gmail.com>
2016-04-12 23:36:59 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@ public:
         bool neg=false;
         if (x < 0) neg = true, x = -x;
         unsigned int ret = 0;
-        while (x / 10 != 0){
+        while (x != 0){
             int temp = ret*10;
             if(temp/10!=ret){return 0;}
             ret = ret*10 + x%10;
